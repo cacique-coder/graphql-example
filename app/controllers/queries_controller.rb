9debfa32bde435c90defe342ce392edf30940140
Daniel Zambrano <daniel@netengine.com.au>
2017-03-17 17:00:54 +1000

    Readable or teachable

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -1,18 +1,5 @@
 class QueriesController < ApplicationController
   def search
-    query_variables = ensure_hash(params[:variables])
-    render json: UserSchema.execute(params[:query], variables: query_variables)
-  end
-
-  private
-  
-  def ensure_hash(query_variables)
-    if query_variables.blank?
-      {}
-    elsif query_variables.is_a?(String)
-      JSON.parse(query_variables)
-    else
-      query_variables
-    end
+    render json: UserSchema.execute(params[:query], variables: params[:variables].to_h)
   end
 end
